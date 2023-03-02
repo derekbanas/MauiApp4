de59ae01e3060835b1d63ef81bfa851c4d85819d
derek <derek@SCARLETTE>
2023-03-02 09:44:44 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void ControlBtn_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new ControlDemo());
+    }
+}
+
